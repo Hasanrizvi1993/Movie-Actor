1ff7989f41d8293b5bcc000cd059a9b8a6deecc5
hasanrizvi1993 <hasanrizvi1993@gmail.com>
2022-04-02 14:45:59 -0700

    added pirctures to the movies page

--- db/create_movies.sql
+++ db/create_movies.sql
@@ -1,4 +1,4 @@
 INSERT INTO main_app_movie(title, release_year, genre, img, created_at)
-VALUES ('Titanic', 1997, 'Romance', 'https://upload.wikimedia.org/wikipedia/en/1/18/Titanic_%281997_film%29_poster.png', );
-INSERT INTO main_app_movie(title, img, age, gender, created_at)
-VALUES ('Spiderman', 2022, 'Romance', 'https://www.filmstories.co.uk/wp-content/uploads/2021/11/spider-man-no-way-home-poster.jpeg');
+VALUES ('Titanic', 1997, 'Romance', 'https://upload.wikimedia.org/wikipedia/en/1/18/Titanic_%281997_film%29_poster.png', '04/02/2022');
+INSERT INTO main_app_movie(title, release_year, genre, img, created_at)
+VALUES ('Spiderman', 2022, 'Romance', 'https://www.filmstories.co.uk/wp-content/uploads/2021/11/spider-man-no-way-home-poster.jpeg', '04/02/2022');
