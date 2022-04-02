6118427c53337468bf25be41f7a936427dd62c5a
hasanrizvi1993 <hasanrizvi1993@gmail.com>
2022-04-02 14:34:18 -0700

    added more things and now talking to the db

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,3 +1,6 @@
-from django.contrib import admin
-
 # Register your models here.
+from django.contrib import admin
+from .models import Movie # import the Cat model from models.py
+# Register your models here.
+
+admin.site.register(Movie) # this line will add the model to the admin panel
--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views.generic.base import TemplateView# <- View class to handle requests
 from django.http import HttpResponse # <- a class to handle sending a type of response
-
+from .models import Movie
 
 # Create your views here.
 
@@ -15,25 +15,12 @@ class About(TemplateView):
 
 
 #movie
-class Movie:
-    def __init__(self, name, age, gender):
-        self.name = name
-        self.age = age
-        self.gender = gender
-
-movies = [
-    Movie("Mau", 5, "Female"),
-    Movie("Garfield", 43, "Male"),
-    Movie("Meowth", 25, "Male"),
-    Movie("Salem", 500, "Male"),
-]
-
-
-
 class MovieList(TemplateView):
     template_name = 'movielist.html'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        movies = Movie.objects.all()
         context["movies"] = movies # this is where we add the key into our context object for the view to use
         return context
+
